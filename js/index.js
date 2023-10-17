f4fa511a1ca99f734b166ad6370599a3d45f8916
Evgeniy Vasiliev <vasilievdevelop@gmail.com>
2023-10-17 15:15:03 +0300

    CV Prod

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// const presentOrderBtn = document.querySelector('.present__order-btn'),
+//       pageOverlayMmodal = document.querySelector('.page__overlay_modal'),
+//       modalClose = document.querySelector('.modal__close');
+
+// const hendlerModal = (openBtn, modal, openSelector, closeTrigger) => {
+//   openBtn.addEventListener('click', () => {
+//     modal.classList.add(openSelector);
+//   });
+
+//   closeTrigger.addEventListener('click', () => {
+//     modal.classList.remove(openSelector);
+//   });
+// };
+
+// hendlerModal(presentOrderBtn, pageOverlayMmodal, 'page__overlay_modal_open', modalClose);
